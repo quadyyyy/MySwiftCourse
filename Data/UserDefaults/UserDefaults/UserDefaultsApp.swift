ff51018b3b448f46ca6ccdb2583e2f9fee3d0b17
Timofei <timofey.kupriyanov@icloud.com>
2026-03-24 19:46:56 +0300

    explore UserDefaults and add practice example

---
+++ Data/UserDefaults/UserDefaults/UserDefaultsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefaultsApp.swift
+//  UserDefaults
+//
+//  Created by Куприянов Тимофей on 24.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct UserDefaultsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
