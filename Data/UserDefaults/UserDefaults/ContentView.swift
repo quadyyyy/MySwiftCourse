520b78ca39c9fa8217d3bbb3cbd1c4f4e05ff646
Timofei <timofey.kupriyanov@icloud.com>
2026-03-24 19:49:35 +0300

    explore UserDefaults

--- Data/UserDefaults/UserDefaults/ContentView.swift
+++ Data/UserDefaults/UserDefaults/ContentView.swift
@@ -7,15 +7,18 @@
 
 import SwiftUI
 
+// @AppStorage - UserDefaults, в идеале держать <~100 кб.
+// В нем хранят какие-то постоянные данные (ну да, ну да, defaults), такие как тема, пройден ли онбординг, мелкие настройки типа уведомлений и тому подобного или, к примеру, перешел ли юзер по ссылкам на соцсети приложения/разраба
+// В целом ограничения по размеру нет, но от размера userdefaults напрямую зависит то, насколько быстро приложение будет запускаться, а это важно.
+
+
 struct ContentView: View {
+    @AppStorage("tap") private var tapCount = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button("Tap count: \(tapCount)") {
+            tapCount += 1
         }
-        .padding()
     }
 }
 
