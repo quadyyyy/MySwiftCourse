7d859951dd87bb9851d7e290975ae4445baf0144
Timofei <timofey.kupriyanov@icloud.com>
2026-03-25 18:26:59 +0300

    explore keychain-swift

---
+++ Data/Keychain/Keychain/KeychainApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeychainApp.swift
+//  Keychain
+//
+//  Created by Куприянов Тимофей on 25.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct KeychainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
